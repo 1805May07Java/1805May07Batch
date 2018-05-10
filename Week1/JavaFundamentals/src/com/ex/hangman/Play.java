6f9342be51fc1666815c36ebd33a2aa7f7402a43
cvikupitz <cvikupit@uoregon.edu>
2018-05-10 07:50:56 -0400

    Reverted week1 hangman example back to previous state

--- Week1/JavaFundamentals/src/com/ex/hangman/Play.java
+++ Week1/JavaFundamentals/src/com/ex/hangman/Play.java
@@ -1,8 +1,5 @@
 package com.ex.hangman;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Play {
@@ -10,31 +7,11 @@ public class Play {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Hangman! Let's begin:");
-		guess(getWord());
-	}
-	
-	// Fetch and return a random word from the dictionary
-	static String getWord() {
-		
-		final int SIZE = 172822; // # of lines in dictionary
-		BufferedReader reader = null;
-		String word = null;
-		try {
-			reader = new BufferedReader(new FileReader("src/com/ex/hangman/words.txt"));
-			int line = new Random().nextInt(SIZE); // Choose random line
-			word = null;
-			for (int i = 0; i <= line; i++) // Go to selected line
-				word = reader.readLine();
-			reader.close();
-			return word.toLowerCase();
-		} catch (Exception e) {
-			return null;
-		}
+		guess("test");
 	}
 
 	static void guess(String word) {
 		String wrong = "";
-		int tries = 9;
 		char[] guessed = new char[word.length()];
 		for(int i = 0; i < word.length(); i++) {
 			guessed[i] = '_';
@@ -49,16 +26,9 @@ public class Play {
 			//Scanner object allows user input
 			Scanner scan = new Scanner(System.in);
 
-			String line = scan.nextLine();
-			String letter = line.toLowerCase().substring(0, 1);
+			String letter = scan.nextLine();
 
-			// User already guessed this, restart loop
-			if (wrong.contains(letter)) {
-				System.out.println("You've already guessed that letter.");
-				continue;
-			}
-			
-			if (word.contains(letter)) {
+			if(word.contains(letter)) {
 				for(int i = 0; i < word.length(); i++) {
 					if(word.charAt(i)==letter.charAt(0)) {
 						guessed[i] = letter.charAt(0);
@@ -67,14 +37,10 @@ public class Play {
 			}
 			else {
 				wrong += letter + " ";
-				if (tries-- <= 0) {
-					System.out.println("You are out of tries! Game Over!");
-					System.out.printf("the word was %s\n", word);
-					scan.close();
-					return;
-				}
 				System.out.println("WRONG!! Your guesses are: " + wrong);
 			}
+			
+			
 
 		}
 		
