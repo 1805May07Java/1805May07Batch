32fde7d862a3527a93cbc464566575cd2511ed0e
DStehleyJr <39063352+DStehleyJr@users.noreply.github.com>
2018-05-10 13:22:20 -0400

    Add files via upload

---
+++ Smallest.java
@@ -0,0 +1,30 @@
+package com.rev.exercises;
+
+import java.util.Scanner;
+
+public class Smallest {
+
+	public static void main(String[] args) 
+	{
+		double a, b, answer;
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter a number.");
+		a = in.nextDouble();
+		
+		System.out.println("Please enter another number.");
+		b = in.nextDouble();
+		
+		if(a == b)
+		{
+			System.out.println("That's the same number");
+		}
+		else
+		{
+			answer = a < b ? a : b;
+			System.out.println(answer + "is the smallest.");
+		}
+	}
+
+}
