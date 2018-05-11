7504034fca7235faeac3e98d0fb83eacaade4364
cvikupitz <cvikupit@uoregon.edu>
2018-05-11 13:48:08 -0400

    Removed dao package from project

--- Cole_Vikupitz_Code/DesignPatterns/src/com/revature/dao/InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-package com.revature.dao;
-
-public class InvalidInputException extends Exception {
-
-	public InvalidInputException() {
-		super("Invalid input, please try again.");
-	}
-}
--- Cole_Vikupitz_Code/DesignPatterns/src/com/revature/dao/Student.java
+++
@@ -1,7 +0,0 @@
-package com.revature.dao;
-
-public class Student {
-
-	private String username, password;
-	private float gpa;
-}
