8443dded8f858bc0782f920f5230e2954fe129c5
cvikupitz <cvikupit@uoregon.edu>
2018-05-11 14:11:39 -0400

    Initial upload of project 1

---
+++ Cole_Vikupitz_Code/Project1/src/com/revature/version1/Driver.java
@@ -0,0 +1,22 @@
+package com.revature.version1;
+
+import java.util.Scanner;
+
+public class Driver {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	/*
+	 * Banking Requirements
+	 * - Create Users
+	 * - Login/logout
+	 * - Create account(s)
+	 * - Update account(s)
+	 * - Persist data in .txt file
+	 */
+
+	public static void main(String[] args) {
+
+		scanner.close();
+	}
+}
