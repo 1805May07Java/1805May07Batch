84a54142b728c732f977c828b93284b4079b73bd
cvikupitz <cvikupit@uoregon.edu>
2018-05-11 20:03:08 -0400

    Added more to project 1, version 1

--- Cole_Vikupitz_Code/Project1/src/com/revature/version1/BankDatabase.java
+++ Cole_Vikupitz_Code/Project1/src/com/revature/version1/BankDatabase.java
@@ -2,7 +2,8 @@
  * BankDatabase.java
  * Author: Cole Vikupitz
  *
- * FIXME - ADD DESCRIPTION
+ * Singleton class that represents the database system for all bank
+ * records. Users can authenticate, retrieve, save, and load bank records.
  */
 
 package com.revature.version1;
@@ -12,47 +13,90 @@ import java.util.HashMap;
 
 public class BankDatabase {
 
+	// Single instance of the database
 	private static BankDatabase instance = null;
+	// Map to store usernames to their corresponding account
 	private HashMap<String, User> map;
 
+	// Private constructor
 	private BankDatabase() {
 
 		this.map = new HashMap<String, User>();
 	}
 
+	/**
+	 * Returns the single instance of the bank account database.
+	 *
+	 * @return The single instance of the bank database.
+	 */
 	public static BankDatabase getInstance() {
 
-		if (instance == null)
+		if (instance == null) // Create new instance if not already
 			instance = new BankDatabase();
 		return instance;
 	}
 
+	/**
+	 * Adds the specified user 'user' instance record into the database.
+	 *
+	 * @param user - The user account record to store in the database.
+	 */
 	public void addUser(User user) {
 
 		this.map.put(user.getUsername(), user);
 	}
 
+	/**
+	 * Returns true/false depending on if the username 'username' already
+	 * exists in the database.
+	 *
+	 * @param username - The username to check for in the database.
+	 * @return True if there exists a record with the specified username,
+	 * false if not.
+	 */
 	public boolean userNameExists(String username) {
 
 		return instance.map.containsKey(username);
 	}
 
+	/**
+	 * Returns the User record corresponding with the specified username and
+	 * password 'username' and 'password', respectively. If the username and
+	 * password fail to authenticate, that is, no such user record exists in
+	 * the database, null is returned.
+	 *
+	 * @param username - The username to authenticate.
+	 * @param password - The password to check.
+	 * @return The User instance with the username and password, null if
+	 * authentication failed.
+	 */
 	public User authenticate(String username, String password) {
 
 		User user = instance.map.get(username);
-		if (user == null)
+		if (user == null) // Username does not exist
 			return null;
-		if (!user.getPassword().equals(password))
+		if (!user.getPassword().equals(password)) // Username and password don't match
 			return null;
 		return user;
 	}
 
+	/**
+	 * Saves the user record database to the specified file 'filePath'.
+	 *
+	 * @param filePath - The file (and path) to save the records to.
+	 */
 	public void saveDBtoFile(String filePath) {
 
 		User[] users = (User[]) instance.map.values().toArray();
 		FileUtility.saveData(users, filePath);
 	}
 
+	/**
+	 * Loads all user account records into the database from the specified
+	 * file 'filePath'.
+	 *
+	 * @param filePath - The file (and path) to load the records from.
+	 */
 	public void loadDBfromFile(String filePath) {
 
 		User[] users = FileUtility.loadData(filePath);
--- Cole_Vikupitz_Code/Project1/src/com/revature/version1/BankMachine.java
+++ Cole_Vikupitz_Code/Project1/src/com/revature/version1/BankMachine.java
@@ -7,17 +7,55 @@
 
 package com.revature.version1;
 
+// Imports
 import java.util.Scanner;
 
 public class BankMachine {
 
 	private Scanner scanner;
+	private User currentUser;
 
 	public BankMachine() {
 
 		this.scanner = new Scanner(System.in);
+		this.currentUser = null;
 	}
 
-	public void run() {}
+	public void run() {
+
+		// Main menu
+	}
+
+	private void createUser() {
+
+		// Prompt username, password, create user, then login when successful
+
+	}
+
+	private void login() {
+
+		// Prompt username, password, login if authenticated
+	}
+
+	private void modifyAccount() {
+
+		// Withdraw, deposit money, logout
+	}
+
+	private int promptNumber(int min, int max) {
+
+		while (true) {
+
+			try {
+				System.out.print("> ");
+				int number = Integer.parseInt(this.scanner.nextLine());
+				if (min <= number && number <= max)
+					return number;
+				System.out.println("Invalid entry, please enter a valid option.");
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid entry, please enter a valid option.");
+			}
+		}
+	}
 
 }
