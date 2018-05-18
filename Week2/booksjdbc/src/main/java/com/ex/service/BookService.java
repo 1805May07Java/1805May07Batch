c7645f19cd9cc8dec1813969d8441488d0cf3289
genesisb17 <genesis.bonds@gmail.com>
2018-05-18 13:06:07 -0400

    pushing current project

--- Week2/booksjdbc/src/main/java/com/ex/service/BookService.java
+++ Week2/booksjdbc/src/main/java/com/ex/service/BookService.java
@@ -2,17 +2,21 @@ package com.ex.service;
 
 import java.util.List;
 
+import com.ex.dao.BookDao;
+import com.ex.dao.Dao;
 import com.ex.pojos.Author;
 import com.ex.pojos.Book;
 
 public class BookService {
 
+	static Dao<Book, Integer> bookdao = new BookDao();
+	
 	public List<Book> getBooksByAuthor(Author a){
 		return null;
 	}
 
 	public List<Book> getAllBooks(){
-		return null;
+		return bookdao.getAll();
 	}
 
 	public void addBookToAuthor(Author a, Book b){
