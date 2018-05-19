52f6e176bd188a4e12e6e474c17d29258108715c
genesisb17 <genesis.bonds@gmail.com>
2018-05-19 12:20:33 -0400

    fridays work

--- Week2/booksjdbc/src/main/java/com/ex/service/BookService.java
+++ Week2/booksjdbc/src/main/java/com/ex/service/BookService.java
@@ -2,28 +2,48 @@ package com.ex.service;
 
 import java.util.List;
 
+import com.ex.dao.BookAuthorDao;
 import com.ex.dao.BookDao;
 import com.ex.dao.Dao;
 import com.ex.pojos.Author;
 import com.ex.pojos.Book;
+import com.ex.pojos.BookAuthor;
 
 public class BookService {
 
 	static Dao<Book, Integer> bookdao = new BookDao();
+	static Dao<BookAuthor, Integer> joindao = new BookAuthorDao();
 	
 	public List<Book> getBooksByAuthor(Author a){
 		return null;
+	}
+	
+	public void update(Book b) {
+		bookdao.update(b);
 	}
 
 	public List<Book> getAllBooks(){
 		return bookdao.getAll();
 	}
 
-	public void addBookToAuthor(Author a, Book b){
-
+	public Book addBook(Book b){
+		return bookdao.save(b);
+	}
+	
+	public void addBookAuthor(Author a, Book b) {
+		joindao.save(new BookAuthor(b, a));
 	}
 
+	public Book getById(int id) {
+		return bookdao.findOne(id);
+	}
 
+	public boolean unique(String isbn) {
+		Book dummy = new Book();
+		dummy.setIsbn(isbn);
+		return bookdao.isUnique(dummy);
+		
+	}
 
 
 }
