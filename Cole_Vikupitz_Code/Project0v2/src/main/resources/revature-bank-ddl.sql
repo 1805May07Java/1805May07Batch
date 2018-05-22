ebbecc27399b1ebc29c42a9bd0fe7fc27e7df3f9
cvikupitz <cvikupit@uoregon.edu>
2018-05-22 16:45:24 -0400

    Uploaded html/css/javascript samples

---
+++ Cole_Vikupitz_Code/Project0v2/src/main/resources/revature-bank-ddl.sql
@@ -0,0 +1,109 @@
+/*
+ * revature-bank-ddl.sql
+ * Author: Cole Vikupitz
+ *
+ * The DDL script for intializing the banking database used in project 0.
+ */
+
+
+/*
+ * Drops the database if it already exists in system
+ */
+DROP USER bank_admin CASCADE;
+
+
+/*
+ * Create the user/database & credentials
+ */
+CREATE USER bank_admin
+IDENTIFIED BY f9344b8fa2b8bd2f
+DEFAULT TABLESPACE users
+TEMPORARY TABLESPACE temp
+QUOTA 10M ON users;
+
+/*
+ * Grant privileges to the new user
+ */
+GRANT CONNECT TO bank_admin;
+GRANT RESOURCE TO bank_admin;
+GRANT CREATE SESSION TO bank_admin;
+GRANT CREATE TABLE TO bank_admin;
+GRANT CREATE VIEW TO bank_admin;
+
+conn bank_admin/f9344b8fa2b8bd2f
+
+
+/*
+ * Defines and creates the tables
+ */
+CREATE TABLE Account_Type (
+    
+    AccountTypeId NUMBER PRIMARY KEY,
+    AccountType VARCHAR2(20) NOT NULL
+);
+
+CREATE TABLE User_Profile (
+
+    UserId NUMBER PRIMARY KEY,
+    FirstName VARCHAR2(25) NOT NULL,
+    LastName VARCHAR2(25) NOT NULL,
+    Username VARCHAR2(25) NOT NULL UNIQUE,
+    Password_ VARCHAR2(25) NOT NULL
+);
+
+CREATE TABLE Account (
+
+    AccountId NUMBER PRIMARY KEY,
+    AccountUser_FK NUMBER NOT NULL,
+    AccountType_FK NUMBER NOT NULL,
+    AccountBalance NUMBER(20, 2) DEFAULT 0,
+    FOREIGN KEY(AccountUser_FK) REFERENCES User_Profile(UserId),
+    FOREIGN KEY(AccountType_FK) REFERENCES Account_Type(AccountTypeId)
+);
+
+
+/*
+ * Add the account types into the table
+ */
+INSERT INTO Account_Type VALUES (1, 'Credit');
+INSERT INTO Account_Type VALUES (2, 'Savings');
+INSERT INTO Account_Type VALUES (3, 'Checking');
+
+
+/*
+ * Create sequence(s); used for primary keys for new records
+ */
+CREATE SEQUENCE USER_SEQ
+START WITH 1
+INCREMENT BY 1;
+
+CREATE SEQUENCE ACCOUNT_SEQ
+START WITH 100
+INCREMENT BY 1;
+
+
+/*
+ * Create all triggers the database will use
+ */
+CREATE OR REPLACE TRIGGER USER_SEQ_TRIGGER
+BEFORE INSERT ON User_Profile
+FOR EACH ROW
+BEGIN
+  SELECT USER_SEQ.NEXTVAL INTO :new.UserId FROM DUAL;
+END;
+/
+
+CREATE OR REPLACE TRIGGER ACCOUNT_SEQ_TRIGGER
+BEFORE INSERT ON Account
+FOR EACH ROW
+BEGIN
+  SELECT ACCOUNT_SEQ.NEXTVAL INTO :new.AccountId FROM DUAL;
+END;
+/
+
+
+/*
+ * Commit all changes, exit
+ */
+COMMIT;
+EXIT;
---
+++ Cole_Vikupitz_Code/Week3-WebDev/bookstore.js
@@ -0,0 +1,41 @@
+
+window.onload = function() {
+	console.log("Testing -- page has loaded");
+	loadGenres();
+}
+
+function loadGenres() {
+
+	var genres = ["Business", "Comedy", "Cooking", "Drama", "Engineering",
+				"Fiction", "History", "Horror", "Non-Ficiton", "Romance", "Science"];
+
+	for (let i = 0; i < genres.length; i++) {
+		var elem = document.createElement("option");
+		elem.value = genres[i].toLowerCase();
+		elem.textContent = genres[i];
+		document.getElementById("genres").appendChild(elem);
+	}
+}
+
+function validateISBN() {
+
+	var input = document.getElementById("isbn").value;
+	var numRegEx = /^[0-9]+$/;
+	if (! (numregEx.text(input) & input.length == 10)){
+		document.getElementById("isbnwarning").hidden = false;
+		document.getElementById("isbnwarning").innerHTML = "Not valid input";
+	} else {
+		document.getElementById("isbnwarning").hidden = true;
+	}
+}
+
+function addBook() {
+
+	var isbn = document.getElementById("isbn").value;
+	var title = document.getElementById("title").value;
+	var price = document.getElementById("price").value;
+	var e = document.getElementById("genres");
+	var genre = e.options[e.selectedIndex].value
+	console.log(genre);
+}
+
