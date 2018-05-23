993303f06c7f4c30ae69dd787f7171c142398601
NAshcroft42 <39060893+NAshcroft42@users.noreply.github.com>
2018-05-23 19:17:34 -0400

    Delete exc10_Ternary.java

--- Nathan_Ashcroft_code/Homework/src/com/rev/questions/exc10_Ternary.java
+++
@@ -1,8 +0,0 @@
-package com.rev.questions;
-
-public class exc10_Ternary {
-	//Find the minimum of two numbers using ternary operators
-	public int tern(int a, int b) {
-		return (a < b) ? a : b;
-	}
-}
