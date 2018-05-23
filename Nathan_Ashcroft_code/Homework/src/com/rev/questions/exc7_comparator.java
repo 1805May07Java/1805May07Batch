c326c5cb72f595a6bb237aa5066fc6c305003fd4
NAshcroft42 <39060893+NAshcroft42@users.noreply.github.com>
2018-05-23 19:19:04 -0400

    Delete exc7_comparator.java

--- Nathan_Ashcroft_code/Homework/src/com/rev/questions/exc7_comparator.java
+++
@@ -1,6 +0,0 @@
-package com.rev.questions;
-
-public class exc7_comparator {
-	//Sort two employees based on their name, department, and age using the Comparator interface.
-	//find out what comparator interface is
-}
