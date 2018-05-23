22e41a5c9bf1a07a4adaae0de721f644590ead97
Nicholas Offutt <nicholasoffutt640@gmail.com>
2018-05-23 16:28:55 -0400

    updating homework

--- Nicholas_Offutt_Code/HtmlWebStuff/FizzBuzz.js
+++ Nicholas_Offutt_Code/HtmlWebStuff/FizzBuzz.js
@@ -1,24 +1,53 @@
 
 
+document.getElementById('FzBz').addEventListener('click', run);
+
+
+function run()
+{
+    var n = document.getElementById('fizzy').value;
+    var arg = fizzBuzz(n);
+    var text = "";
+    for(let i = 1; i< arg.length; i++)
+    {
+        console.log(arg[i]);
+        text = text + ' ' +arg[i];
+    }
+    document.getElementById('landing zone').innerHTML = text;
+}
 
 
 function fizzBuzz(input)
 {
-    var fizz = 'Fizz';
-    var buzz = 'Buzz';
+    
+    var arr = [];
 
     for(let i = 1; i <=input; i++)
     {
-
+		if (i%15 === 0)
+		{
+			arr[i] = "FizzBuzz";
+		}
+		else if(i % 5 ===0 )
+		{
+			arr[i] = "Buzz";
+		}
+		else if(i % 3 ===0)
+		{
+			arr[i] = "Fizz";
+		}
+		else
+		{
+			arr[i] = i;
+		}
     }
-
+    
+    return arr;
 
 }
 
 
-
-
-function validateISBN(){
+function validateInput(){
 
     var input = document.getElementById("fizzy").value;
 
