c44f2a857a54ae6a91a6104d540da5a35117e65f
NAshcroft42 <39060893+NAshcroft42@users.noreply.github.com>
2018-05-23 19:18:03 -0400

    Delete exc16_length.java

--- Nathan_Ashcroft_code/Homework/src/com/rev/questions/exc16_length.java
+++
@@ -1,8 +0,0 @@
-package com.rev.questions;
-
-public class exc16_length {
-	public static void main(String[] args) {
-		
-		System.out.println("Number of charectors entered: " + args[0].length());
-	}
-}
