a190756350914f3ee0e5eb1fa0b9033f83e6c1fc
unknown <mebryant@millersville.edu>
2018-05-24 16:51:06 -0400

    ok

--- Week3/AJAX101.js
+++ Week3/AJAX101.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-window.onload= function(){
-    loadGenres();
-
-
-
-}
-//This functionwill load in our genres from our API
-// to populate the drop down
-    function loadGenres(){
-            var xhr = new XMLHttpRequest();
-             xhr.onreadystatechange = function(){
-                    if(xhr.readyState == 4 && xhr.status == 200){
-                        var genres = JSON.parse(xhr.responseText);
-                        for(let i = 0; i < genres.length; i++){
-                                var elem = document.createElement("option");
-                                elem.value = genres[i].id;
-                                elem.value = genres[i].name;
-                                elem.innerHTML = genres[i].name;
-                                $('#genres').append(elem);
-                        }
-
-                    }
-
-            }
-            xhr.open("GET", "http://localhost:3000/genres/", true);
-            xhr.send();
-    }
-=======
 window.onload = () => {
     getBooks();
 }
@@ -50,4 +21,4 @@ function getBooks(){
     //STEP 4: SEND
     xhr.send();
 }
->>>>>>> master
+
