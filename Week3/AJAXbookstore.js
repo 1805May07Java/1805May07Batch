26af0ab3c9ed5cb8c38eefffd0419241a9ead9b9
genesisb17 <genesis.bonds@gmail.com>
2018-05-24 15:52:58 -0400

    more on ajax

---
+++ Week3/AJAXbookstore.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+    loadGenres();
+}
+
+/*
+This function will load in our genres from our API to 
+populate the drop down
+*/
+function loadGenres(){
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState==4 && xhr.status == 200){
+            var genres = JSON.parse(xhr.responseText);
+            for(let i = 0; i < genres.length; i++){
+                var elem = document.createElement("option");
+                elem.value = genres[i].id;
+                elem.innerHTML = genres[i].name;
+                $('#genres').append(elem);
+            }
+        }
+    }
+
+    xhr.open("GET", "http://localhost:3000/genres", true);
+    xhr.send();
+}
