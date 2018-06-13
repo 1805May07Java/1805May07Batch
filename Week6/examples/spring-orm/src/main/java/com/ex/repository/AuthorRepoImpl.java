6854a589ae5e64a1c9211d64a181762b4b2a73a2
genesisb17 <genesis.bonds@gmail.com>
2018-06-13 11:03:43 -0400

    Data persistence with Spring ORM

---
+++ Week6/examples/spring-orm/src/main/java/com/ex/repository/AuthorRepoImpl.java
@@ -0,0 +1,37 @@
+package com.ex.repository;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ex.beans.Author;
+
+@Transactional
+@Repository
+public class AuthorRepoImpl implements AuthorRepository{
+
+	@Autowired
+	SessionFactory sf;
+	
+	@Override
+	public List<Author> getAll() {
+		return sf.getCurrentSession().createCriteria(Author.class).list();
+	}
+
+	@Override
+	public Author getById(int id) {
+		return (Author) sf.getCurrentSession().get(Author.class, id);
+	}
+
+	@Override
+	public Author add(Author a) {
+		int id = (Integer) sf.getCurrentSession().save(a);
+		a.setId(id);
+		return a;
+	}
+
+}
---
+++ Week6/examples/spring-orm/src/main/java/com/ex/run/App.java
@@ -0,0 +1,35 @@
+package com.ex.run;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ex.beans.Author;
+import com.ex.beans.Book;
+import com.ex.beans.Genre;
+import com.ex.repository.BookRepository;
+
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		BookRepository repo = ac.getBean("bookRepository", BookRepository.class);
+		
+		Book b = new Book("2233445566", "The Illiad", 85.00);
+		Author a = new Author("Homer", "HisLastName", "World reknown author");
+		Author a2 = new Author("Test", "Author", "ipsum");
+		Set<Author> authors = new HashSet<Author>();
+		authors.add(a);
+		authors.add(a2);
+		b.setAuthors(authors);
+		
+		Genre g = new Genre("Classic");
+		b.setGenreId(g);
+		
+		repo.add(b);
+		
+	}
+
+}
